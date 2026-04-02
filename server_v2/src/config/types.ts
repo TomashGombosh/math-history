525c342045c193c1c26daf15ae75928498aa1566
Tomash Gombosh <blackjneco@gmail.com>
2026-04-02 22:13:49 +0000

    (feature): Deploy server using serverless (#6)

--- server_v2/src/config/types.ts
+++ server_v2/src/config/types.ts
@@ -2,8 +2,4 @@ export type NodeEnv = 'test' | 'local' | 'dev' | 'development' | 'stage' | 'prod
 
 export interface AppConfig {
 	nodeEnv: NodeEnv;
-	jwtSecret: string;
-	jwtExpiresIn: string;
-	adminUsername: string;
-	adminPassword: string;
 }
--- server_v2/src/modules/api/auth/login/post/schema.ts
+++
@@ -1,6 +0,0 @@
-import z from 'zod';
-
-export const schema = z.object({
-	username: z.string(),
-	password: z.string(),
-});
