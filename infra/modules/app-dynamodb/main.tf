edb2f90d983bb4a9257823cfe10f709dbf6df40f
TomashGombosh <blackjneco@gmail.com>
2026-04-02 21:25:10 +0300

    (infra): Create resource by terraform

---
+++ infra/modules/app-dynamodb/main.tf
@@ -0,0 +1,52 @@
+locals {
+  table_name = "${var.project_name}-ddb-${var.environment}"
+  default_tags = merge(
+    {
+      Project     = var.project_name
+      Environment = var.environment
+      ManagedBy   = "terraform"
+    },
+    var.tags,
+  )
+}
+
+resource "aws_dynamodb_table" "main" {
+  name         = local.table_name
+  billing_mode = "PAY_PER_REQUEST"
+
+  hash_key  = "pk"
+  range_key = "sk"
+
+  attribute {
+    name = "pk"
+    type = "S"
+  }
+
+  attribute {
+    name = "sk"
+    type = "S"
+  }
+
+  attribute {
+    name = "gsi1pk"
+    type = "S"
+  }
+
+  attribute {
+    name = "gsi1sk"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name            = "GSI1"
+    hash_key        = "gsi1pk"
+    range_key       = "gsi1sk"
+    projection_type = "ALL"
+  }
+
+  point_in_time_recovery {
+    enabled = var.enable_point_in_time_recovery
+  }
+
+  tags = local.default_tags
+}
