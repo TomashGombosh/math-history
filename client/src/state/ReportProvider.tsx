45635a48142048dbd2397b0a5545709083ae35f1
SemViX <vitalsem.1973@gmail.com>
2026-06-13 18:53:26 +0300

    (feat):add ReportProvider

---
+++ client/src/state/ReportProvider.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useMemo, useState, type ReactNode } from "react";
+import { ReportContext } from "./ReportContenxt";
+
+type Props = {
+    children: ReactNode;
+}
+
+const ReportProvider = ({children}:Props) => {
+    const [hoveredComponent, setHoveredComponent] = useState<string | null>(null);
+    const [open, setOpen] = useState<boolean>(false)
+
+    useEffect(() => {
+        const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+        if (isMobile) return;
+
+        const handleKeyDown = (e: globalThis.KeyboardEvent) => {
+            if (e.key === "r" && e.ctrlKey) {
+                e.preventDefault();
+
+                if (hoveredComponent){
+                    setOpen(true);
+                }
+            }
+        };
+
+        window.addEventListener("keydown", handleKeyDown);
+        return () => {window.removeEventListener("keydown", handleKeyDown);
+        }
+    }, [hoveredComponent]);
+
+    const value = useMemo(() => ({
+        hoveredComponent,
+        setHoveredComponent,
+    }), [hoveredComponent]);
+
+  return (
+    <ReportContext.Provider value={value}>
+        {children}
+    </ReportContext.Provider>
+  )
+}
+
+export default ReportProvider
